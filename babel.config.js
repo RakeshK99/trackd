ffb3f7bdd0967136e0da70dcaf7d344e7b9b292a
Rakeshk99 <rakesh.vkottapalli@gmail.com>
2026-05-30 21:02:52 -0400

    v1

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,8 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-    plugins: ['react-native-reanimated/plugin'],
+    // Reanimated v4 moved the worklets transform to react-native-worklets.
+    // This plugin must remain LAST in the list.
+    plugins: ['react-native-worklets/plugin'],
   };
 };
